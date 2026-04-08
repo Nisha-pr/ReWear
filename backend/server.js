147cc7777851f1657ef3ab4915e972f49b6f6006
Nisha-pr <nisha45688@gmail.com>
2026-04-08 11:17:32 +0530

    Fix CORS multiple origins

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,10 @@ setupSocket(io);
 // ─── Middleware ──────────────────────────────────────────────────────────────
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: [
+      process.env.FRONTEND_URL || 'http://localhost:5173',
+     'http://localhost:5173',
+  ],
     credentials: true,
   })
 );
